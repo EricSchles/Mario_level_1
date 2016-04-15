d930078b0dfc6f543bfa1b2e6f5ef9bc815b4308
Eric Schles <ericschles@gmail.com>
2016-04-15 12:24:38 -0400

    first commit

---
+++ mario_level_1/mario_level_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+__author__ = 'justinarmstrong'
+
+"""
+This is an attempt to recreate the first level of
+Super Mario Bros for the NES.
+"""
+
+import sys
+import pygame as pg
+from data.main import main
+import cProfile
+
+def run_game():
+    main()
+    pg.quit()
+    sys.exit()
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="mario",
+    version="1.0",
+    description="mario!",
+    author="not me!",
+    packages=["mario_level_1"]
+)
